c2e3b08b064ca4f14d4eb59d8f8eac4c3d6316ef
Emma <emma.farineaux@epitech.eu>
2024-01-11 13:09:35 +0100

    [~] update -> update file(s)

--- push_that.sh
+++ push_that.sh
@@ -16,7 +16,7 @@ else
     handle_error "gitignore not found."
 fi
 
-read -i "" -p "Do you have specific files to add ? (y/n): " add_files
+read -e -i "" -p "Do you have specific files to add ? (y/n): " add_files
 
 if [ "$add_files" == "y" ] || [ "$add_files" == "Y" ]; then
     read -e -i "" -p "Please enter space-separated list of files to add: " files_to_add
